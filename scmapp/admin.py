29a789737897a884d9aa21c0392829e692e3ca88
Afna sherin <80709664+afna34@users.noreply.github.com>
2021-05-25 14:17:31 +0530

    deleted

--- scmapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Admin, Event, Book_ground
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Admin)
-admin.site.register(Event)
-admin.site.register(Book_ground)
--- scmapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScmappConfig(AppConfig):
-    name = 'scmapp'
