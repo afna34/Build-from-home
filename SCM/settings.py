6b7e049d57951784b4c6af98b922658022efb232
Khadeeja Minha <khadeejaminha2002@gmail.com>
2021-05-26 12:49:26 +0530

    settings.py

--- SCM/settings.py
+++ SCM/settings.py
@@ -134,4 +134,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
+)
