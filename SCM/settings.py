b69fc4357e97760f3351812fc686b91e992b48ee
Khadeeja Minha <khadeejaminha2002@gmail.com>
2021-05-26 12:47:28 +0530

    settings.py

--- SCM/settings.py
+++ SCM/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
