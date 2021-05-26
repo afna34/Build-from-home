438ffbf97916f7fa708b0cc422d67d1175edf6dd
Khadeeja Minha <khadeejaminha2002@gmail.com>
2021-05-26 14:38:19 +0530

    settings.py

--- SCM/settings.py
+++ SCM/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATIC_DIRS = [
     STATIC_DIR,
 ]
+
 
