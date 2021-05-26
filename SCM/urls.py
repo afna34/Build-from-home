6a3cc6241dd13f4cad53df7f0755911b0afa363e
Khadeeja Minha <khadeejaminha2002@gmail.com>
2021-05-26 11:49:01 +0530

    Deploy

--- SCM/urls.py
+++ SCM/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from scmapp import views
 
 urlpatterns = [
     # path(r'^/',views.index,name='index'),
