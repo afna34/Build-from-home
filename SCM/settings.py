f9b4ee0588842f2fc148dfa6031ee242d0f62912
Khadeeja Minha <khadeejaminha2002@gmail.com>
2021-05-26 12:40:52 +0530

    settings.py

--- SCM/settings.py
+++ SCM/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_DIR = os.path.join(BASE_DIR,"static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
